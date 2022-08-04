98fbc6e6f77abe5cd4249ef23c962ec6623ce343
Shlk Sobremesa <shelakh@casa.com>
2022-08-04 15:36:40 +0200

    ft_strcmp created for map_creating

---
+++ lib/libft/ft_strcmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aarribas <aarribas@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/24 10:21:35 by aarribas          #+#    #+#             */
+/*   Updated: 2022/08/03 10:19:46 by aarribas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strcmp(const char *str1, const char *str2)
+{
+	size_t			i;
+	unsigned char	*s1;
+	unsigned char	*s2;
+
+	s1 = (unsigned char *)str1;
+	s2 = (unsigned char *)str2;
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] && s2[i])
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
